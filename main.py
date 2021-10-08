c5163d187a1aefbeecde0b4cd6ad6faa24e8a43d
Héctor <hfaildeestevez@danielcastelao.org>
2021-10-08 13:55:16 +0200

    ejercicio resuelto

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-# parte1
+
+l = (3, 4)
+l2 = (5, 4)
 
 
-fichas1 = (3, 4)
-fichas2 = (4, 5)
-
-
-def encaje(combinacion1, combincacion2):
-    if combinacion1[1] == combincacion2[0] or combinacion1[1] == combincacion2[1]:
-
+# 1
+def encaja1(lista1, lista2):
+    if set(lista1) & set(lista2):
         return "encaja"
     else:
         return "no encaja"
 
 
-print(encaje(fichas1, fichas2))
+print(encaja1(l, l2))
 
-# parte 2
+# 2
+l3 = "3-4"
+l4 = "2-5"
 
+
+def encaja2(ls1, ls2):
+    lista1 = ls1.split("-")
+    lista2 = ls2.split("-")
+    if set(lista1) & set(lista2):
+        return "encaja"
+    else:
+        return "no encaja"
+
+
+print(encaja2(l3, l4))
